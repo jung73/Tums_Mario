50026e8e0796d79c05efd6e00ac657d3f8a53715
jung73 <tender4703@gmail.com>
2024-02-15 18:43:48 +0900

    굼바 수정

--- Assets/DJ/Goomba.cs
+++ Assets/DJ/Goomba.cs
@@ -3,9 +3,9 @@ using UnityEngine.AI;
 
 public class goomba : MonoBehaviour
 {
-    public float movementSpeed = 5f; // 굼바의 이동 속도
+    public float movementSpeed = 2f; // 굼바의 이동 속도
     public float rotationSpeed = 15f; // 굼바의 회전 속도
-    public float detectionRadius = 30f; // 마리오를 감지할 반경, 추후 변경 필요
+    public float detectionRadius = 10f; // 마리오를 감지할 반경, 추후 변경 필요
 
 
     private Rigidbody rb;
@@ -23,7 +23,7 @@ public class goomba : MonoBehaviour
     {
         animator.Play("Wait");
         rb = GetComponent<Rigidbody>();
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Mario");
         // "Mario" 태그로 설정된 오브젝트 찾기, hierachy에서 마리오에 태그 설정
 
     }
@@ -72,7 +72,7 @@ public class goomba : MonoBehaviour
     // 플레이어와 충돌했을 때 호출되는 함수
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Mario"))
         {
             // 만약 마리오가 굼바 위에 있고, 아래 방향으로 내려온다면
             if (collision.contacts[0].normal.y > 0.5f)
